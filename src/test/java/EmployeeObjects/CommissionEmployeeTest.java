30bf6671bced2c77145e96b9190f7c3f18636251
Brendan-Mac-sketch <brendanmcnamee97@gmail.com>
2025-03-15 21:00:11 -0500

    created the tests for the salary employed and passed each

--- src/test/java/EmployeeObjects/CommissionEmployeeTest.java
+++ src/test/java/EmployeeObjects/CommissionEmployeeTest.java
@@ -10,15 +10,19 @@ class CommissionEmployeeTest {
 
     @Test
     void testIncreaseSales() {
-
+        CommissionEmployee emp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", .0265);
 
     }
 
     @Test
     void testAnnualRaise() {
+        CommissionEmployee emp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", .0265);
+        emp.annualRaise();
     }
 
     @Test
     void testHolidayBonus() {
+        CommissionEmployee emp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", .0265);
+        assertEquals(emp.holidayBonus(),0);
     }
 }
