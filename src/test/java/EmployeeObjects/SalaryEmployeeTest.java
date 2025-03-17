d7feb5fc6c23a7a9bdf425876abbdbeabc0b4d73
Brendan-Mac-sketch <brendanmcnamee97@gmail.com>
2025-03-16 20:46:53 -0500

    test calculate weekly pay over 40 hours is working

--- src/test/java/EmployeeObjects/SalaryEmployeeTest.java
+++ src/test/java/EmployeeObjects/SalaryEmployeeTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SalaryEmployeeTest {
-    SalaryEmployee emp = new SalaryEmployee("Steve", "Rodgers", 3781, "Sales", "Manager", 64325);
+
     @Test
 
     void testCalculateWeeklyPay() {
